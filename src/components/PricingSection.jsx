e826174e3a31948132988522474a9e04c2b25402
Hazem El-sokary <hazemessam81999@gmail.com>
2025-12-23 22:15:26 +0200

    update link

--- src/components/PricingSection.jsx
+++ src/components/PricingSection.jsx
@@ -3,7 +3,7 @@ import './PricingSection.css';
 
 // رابط Stripe Payment Link
 // const STRIPE_PAYMENT_LINK = "https://buy.stripe.com/4gM4gA2pY3Up0yLaQ0dAk23";
-const STRIPE_PAYMENT_LINK = "https://buy.stripe.com/fZubJ2fcKbmR3KX5vGdAk24";
+const STRIPE_PAYMENT_LINK = "https://buy.stripe.com/eVqdRa9SqfD7epB7DOdAk25";
 
 const PricingSection = () => {
   const [timeLeft, setTimeLeft] = useState({
